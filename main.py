451e8dc82c092cdf5c5aeda8b31a59421198458f
AS TG <126592884+TGxIRFAN@users.noreply.github.com>
2025-06-22 22:23:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,62 @@
 #=========================================================================
-# [AutoDelete - Telegram bot to delete messages after specific time]      
-# Copyright (C) 2022 Arunkumar Shibu                       
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# [AutoDelete - Telegram bot to delete messages after specific time]
+# Copyright (C) 2022 Arunkumar Shibu
+# License: AGPL-3.0
 #=========================================================================
 
-from time import time 
-from utils.info import *
-from utils.database import *
+from time import time
+from utils.info import *  # Should define SESSION, BOT_TOKEN, CHATS, etc.
+from utils.database import save_message
 from subprocess import Popen
 from pyrogram import Client, filters
 from pyrogram import utils as pyroutils
+import asyncio
 
 pyroutils.MIN_CHAT_ID = -999999999999
 pyroutils.MIN_CHANNEL_ID = -100999999999999
 
-User = Client("auto-delete-user",
-              session_string=SESSION)
+# Initialize User and Bot Clients
+User = Client("auto-delete-user", session_string=SESSION)
+Bot = Client("auto-delete-bot", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
 
-@User.on_message(filters.chat(CHATS))
-async def delete(user, message):
+# Common delete handler
+@filters.chat(CHATS)
+async def delete_handler(client, message):
     try:
-       if bool(WHITE_LIST):
-          if message.from_user.id in WHITE_LIST:
-             return 
-       if bool(BLACK_LIST):
-          if message.from_user.id not in BLACK_LIST:
-             return
-       _time = int(time()) + TIME 
-       save_message(message, _time)
+        if WHITE_LIST and message.from_user and message.from_user.id in WHITE_LIST:
+            return
+        if BLACK_LIST and message.from_user and message.from_user.id not in BLACK_LIST:
+            return
+        _time = int(time()) + TIME
+        save_message(message, _time)
     except Exception as e:
-       print(str(e))
+        print(f"Error: {e}")
 
-@User.on_message(filters.regex("start") & filters.private)
-async def start(user, message):
+User.add_handler(filters.chat(CHATS), delete_handler)
+Bot.add_handler(filters.chat(CHATS), delete_handler)
+
+# Start handler only for bot
+@Bot.on_message(filters.command("start") & filters.private)
+async def start(bot, message):
     await message.reply("Hi, I'm alive!")
 
-#==========================================================
-
+# Launch web server and deletion worker
 Popen(f"gunicorn utils.server:app --bind 0.0.0.0:{PORT}", shell=True)
 Popen("python3 -m utils.delete", shell=True)
-User.run()
+
+# Run both clients
+async def main():
+    await asyncio.gather(
+        User.start(),
+        Bot.start(),
+        idle_loop()
+    )
+
+async def idle_loop():
+    from pyrogram.idle import idle
+    await idle()
+    await User.stop()
+    await Bot.stop()
+
+if __name__ == "__main__":
+    asyncio.run(main())
