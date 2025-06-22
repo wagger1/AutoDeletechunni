7af485088ff6d5d48217ea6a480fcfc68ed54577
AS TG <126592884+TGxIRFAN@users.noreply.github.com>
2025-06-22 23:19:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,9 +44,6 @@ async def delete_handler_bot(client, message):
     except Exception as e:
         print(f"Error in bot handler: {e}")
 
-User.add_handler(filters.chat(CHATS), delete_handler_user)
-Bot.add_handler(filters.chat(CHATS), delete_handler_bot)
-
 # Start handler only for bot
 @Bot.on_message(filters.command("start") & filters.private)
 async def start(bot, message):
